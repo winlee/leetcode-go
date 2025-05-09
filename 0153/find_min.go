5e90a693ab9b70a153ab2a85ed89c10302414503
lizhengxing <lizhengxing@shizhuang-inc.com>
2025-05-09 16:11:18 +0800

    coding

--- 0153/find_min.go
+++ 0153/find_min.go
@@ -69,3 +69,14 @@ func findMin(nums []int) int {
 
 	return nums[low]
 }
+
+func moveZeroes(nums []int) {
+	left, right, n := 0, 0, len(nums)
+	for right < n {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+		right++
+	}
+}
